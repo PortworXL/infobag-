0ffc963c82e78721524aa3a902d66fd6585dd9bd
PortworXL <90553345+PortworXL@users.noreply.github.com>
2022-12-21 21:42:55 +0000

    Fixed white space

--- spec/coin_integration_spec.rb
+++ spec/coin_integration_spec.rb
@@ -4,3 +4,6 @@ require ('./app')
 
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the coin path', {:type => :feature}) do
+  it ('Takes user input number and returns coin results.') do
