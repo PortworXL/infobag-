101eb011aa2d9a102ebad7a893024d351f67b13a
PortworXL <90553345+PortworXL@users.noreply.github.com>
2022-12-23 07:53:05 +0000

    Documentation enhancements

--- spec/coin_integration_spec.rb
+++ spec/coin_integration_spec.rb
@@ -7,3 +7,6 @@ set(:show_exceptions, false)
 
 describe('the coin path', {:type => :feature}) do
   it ('Takes user input number and returns coin results.') do
+    visit('/')
+    fill_in('number', :with => '110')
+    click_button('Submit!')
