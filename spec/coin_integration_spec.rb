1c5991e122ab06265d38d0076479e522906fd7a4
PortworXL <90553345+PortworXL@users.noreply.github.com>
2022-12-29 10:30:19 +0000

    Requirements

--- spec/coin_integration_spec.rb
+++ spec/coin_integration_spec.rb
@@ -10,3 +10,4 @@ describe('the coin path', {:type => :feature}) do
     visit('/')
     fill_in('number', :with => '110')
     click_button('Submit!')
+    expect(page).to have_content('Here is your coin combination 110 cents is 4 quarters, 1 dimes, 0 nickels, 0 pennies')
