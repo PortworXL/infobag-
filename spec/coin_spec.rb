039444090b07d9f867b40d9a8d382cc1a5bfa217
PortworXL <90553345+PortworXL@users.noreply.github.com>
2023-01-30 13:36:01 +0000

    small css fixes

--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -1,3 +1,8 @@
 
 require ('rspec')
 require ('coin.rb')
+
+describe('String#coin') do
+  # it('takes user input for number of cents and returns number of cents') do
+  #   expect(4.coin()).to(eq(4))
+  # end
