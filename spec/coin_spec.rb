0da376a7dd8747a868298657184a68da8cbc5747
PortworXL <90553345+PortworXL@users.noreply.github.com>
2023-02-12 13:22:14 +0000

    Adding badge

--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -6,3 +6,7 @@ describe('String#coin') do
   # it('takes user input for number of cents and returns number of cents') do
   #   expect(4.coin()).to(eq(4))
   # end
+
+  it('divides the user input by 25 and rounds down to return the number of quarters') do
+    expect("25".coin()).to(eq([1,0,0,0]))
+  end
